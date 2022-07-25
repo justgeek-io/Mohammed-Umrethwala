a07a64bcaaf4004e3212fa3fb0e2643b140652c0
root <root@centos7.linuxvmimages.local>
2022-07-25 01:52:14 -0400

    update exp

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,8 +25,8 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+  <li class="nav-item nav-item-hover"><a class="nav-link" href="https://forms.gle/Q9sCj38akFv2rAqi8" target="_blank">Resume</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://justgeek.io" target="_blank">Blog</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,25 +6,33 @@ const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
     title: "Site Reliability Engineer",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
+    cardImage: "assets/images/experience-page/forcepoint.png",
     place: "Forcepoint",
     time: "(June, 2021 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    desp: "<li>Responsible for uptime of Forcepoint products.</li> <li>Monitoring & Managing the product servers",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Site Reliability Engineer",
+    cardImage: "assets/images/experience-page/qualy.png",
+    place: "Qualys",
+    time: "(July 2016 - July 2021)",
+    desp: "<li>Responsible for deployments of the code.<li>Responsible for Monitoring and Managing the product servers</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Senior Technial Support Engineer",
+    cardImage: "assets/images/experience-page/aress.png",
+    place: "Aress Software",
+    time: "(Nov 2011 - July 2016)",
+    desp: "<li>System administration of Linux based web servers.</li><li> Securing server, by installing firewall, configuring firewall and checking security aspects</li>",
   },
+ {
+    title: "System Admin",
+    cardImage: "assets/images/experience-page/geeks.png",
+    place: "Geeks Solutions",
+    time: "(April 2008 - Nov 2011)",
+    desp: "<li>Trouble shooting problems related to FTP, DNS, Mail servers, </li><li> Investigation on various and spamming attack.</li>",
+  },
+
 ];
 
 const showCards2 = () => {
