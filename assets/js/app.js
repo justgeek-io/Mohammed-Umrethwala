7323984afe6c884d931830a23f4399a192fd520a
root <root@centos7.linuxvmimages.local>
2022-07-25 00:30:28 -0400

    deleted Projects from app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,6 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
-   //<li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="https://justgeek.io" target="_blank">Blog</a></li>
